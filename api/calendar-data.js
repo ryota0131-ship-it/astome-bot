96e2359e5dde1ceb67fb32b88e1d9326a8f9dc99
ryota0131-ship-it <ryota.0131@gmail.com>
2026-06-29 10:11:01 +0900

    Update calendar-data.js

--- api/calendar-data.js
+++ api/calendar-data.js
@@ -13,6 +13,8 @@ const redis = {
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
 
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
